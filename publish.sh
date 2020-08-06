c070335baa6ea53428946835526e3bb5b312928a
KreativJos <jos@kreativsoftware.com>
2020-08-06 22:55:31 +0200

    [Fix] Fixed publish script

--- publish.sh
+++ publish.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-npm run test | grep 'fail' &> /dev/null
+test_output="$(npm run test)"
 
-if [$? == 0]; then
+if [[ $test_output == *"failing"* ]]; then
+    echo "Failing test found, cannot publish. Test output:"
+    echo "$test_output"
+else
     vsce package
     sed -e 's/"publisher": "kreativ-software"/"publisher": "jsw"/g' ./package.json >./package.json.temp
     mv package.json package.json.2.temp
@@ -10,6 +13,4 @@ if [$? == 0]; then
     ovsx publish -p $(cat ovsx.key)
     rm package.json
     mv package.json.2.temp package.json
-else
-    echo 'Test failed, cannot publish'
 fi
